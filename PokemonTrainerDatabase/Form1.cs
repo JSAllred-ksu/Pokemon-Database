9eda04024d92cd1e48aaf6de1a9bb5b832774d3b
Samuel Allred <jsallred@ksu.edu>
2023-11-27 10:42:10 -0600

    Results box populates, filters need added

--- PokemonTrainerDatabase/Form1.cs
+++ PokemonTrainerDatabase/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace PokemonTrainerDatabase
 {
     public partial class Form1 : Form
     {
+        SqlConnection Connection;
+        string ConnectionString;
+
         public Form1()
         {
             InitializeComponent();
+            ConnectionString = ConfigurationManager.ConnectionStrings["PokemonTrainerDatabase.Properties.Settings.PokemonTrainerDatabaseTablesConnectionString"].ConnectionString;
+            PopulateResults();
+        }
+
+        private void PopulateResults()
+        {
+            using (Connection = new SqlConnection(ConnectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Pokemon", Connection))
+            {
+                DataTable PokemonTable = new DataTable();
+                adapter.Fill(PokemonTable);
+
+                ResultsBox.DisplayMember = "PokemonName";
+                ResultsBox.ValueMember = "Id";
+                ResultsBox.DataSource = PokemonTable;
+            }
         }
     }
 }
